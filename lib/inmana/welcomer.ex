3b596f90ca40e3dcc2e9978555da1630fc386a0f
PhPierceBrosnan <brunocalmx@gmail.com>
2021-04-21 19:00:11 -0300

    inserting restaurants and handling some errors

--- lib/inmana/welcomer.ex
+++ lib/inmana/welcomer.ex
@@ -1,9 +1,4 @@
 defmodule Inmana.Welcomer do
-  # receber nome e idade do usuario, se usuario se chamar 'banana' e tiver idade '42', recebe uma mensagem especial
-  # se for maior de idade, recebe mensagem normal
-  # se for menor de idade, retorna erro
-  # nome do usuario é tratado para entradas erradas
-
   def welcome(%{"name" => name, "age" => age}) do
     age = age |> String.to_integer()
     name |> String.trim() |> String.downcase() |> evaluate(age)
@@ -17,7 +12,7 @@ defmodule Inmana.Welcomer do
     {:ok, "welcome #{name}"}
   end
 
-  defp evaluate(name, age) do
+  defp evaluate(name, _age) do
     {:error, "you shall not pass #{name}"}
   end
 end
